809e867b560ccda687ab771a99e28bc9dddfc9ef
d819r197 <d819r197@ku.edu>
2019-05-10 16:18:10 -0500

    Transition to reading one tag at a time

--- CacheCalculator/main.cpp
+++ CacheCalculator/main.cpp
@@ -6,15 +6,26 @@
 #include <array>
 #include <thread>
 #include <chrono>
+#include <vector>
 
+//Globar Variables
+  //Global Timer
+  clock_t t;
+  std::vector<string[3]> transactions;
+
+  float getTime() {
+    t = clock();
+    float seconds = (((float)t)/CLOCKS_PER_SEC);
+
+    return(seconds*100);
+  }
 
 struct Proccessor {
   //Proccessor Vars
   int lines;
   int valueIndex;
-  std::string** importTable;
-  std::string** tTable;
   std::string filePath;
+  std::string** importTable = new std::string*[4];
 
   //Proccessor Functions
   //Returns Number of Lines in a Given File
@@ -34,57 +45,6 @@ struct Proccessor {
       std::cout << "Error opening file";
     }
     return (0);
-  }
-
-  //Returns Ptr to 2D array of Size 3xcountLines(filePath)
-  std::string** tagTable(std::string filePath) {
-    //Declare Variables
-    std::string** importTable = new std::string*[4];
-    std::string word = "";
-    std::string line = "";
-    std::ifstream file;
-      file.open(filePath, std::ifstream::in);
-
-    //Open File
-    if (file.is_open()) {
-        int rows = 0;
-
-        //Break File into Lines
-        while(getline(file, line)) {
-          int cols = 0;
-          //Count Number of Lines
-          importTable[rows] = new std::string[countLines(filePath)];
-
-          //Remove Endline char
-          line[line.size()] = ' ';
-
-          //Make Stream Obj of String
-          std::stringstream ssLine(line);
-
-          //Break Lines into Words
-          while(getline(ssLine, word, ' ')) {
-            // std::cout<<word <<" (" <<rows <<',' <<cols <<") | ";
-
-            //Slow Down Proccess to Avoid Error: Incorrect checksum for freed object
-            std::this_thread::sleep_for (std::chrono::nanoseconds(250));
-
-            //Assign Word to importTable
-            importTable[rows][cols] = word;
-              cols++;
-
-            //Initilize State
-            importTable[rows][cols] = "i";
-          }
-          // std::cout<<"\n------------------------------------------------\n";
-          rows++;
-        }
-      return (importTable);
-    }
-    else {
-        std::cout << "Error opening file";
-    }
-      file.close();
-      return(nullptr);
   }
 
   //Updates Proccessor State
@@ -107,21 +67,125 @@ struct Proccessor {
   Proccessor(std::string path) {
     filePath = path;
     valueIndex = 0;
-    lines = countLines(filePath);
-    importTable = tagTable(filePath);
-    std::string** tTable = new std::string*[4];
-
-    for(int rows = 0; rows < lines; rows++) {
-      tTable[rows] = new std::string[lines];
-
-      //Slow Down Proccess to Avoid Error: Incorrect checksum for freed object
-      std::this_thread::sleep_for (std::chrono::nanoseconds(250));
-    }
+    string currentTag[3];
+    //lines = countLines(filePath);
+    //importTable = startImport(filePath);
+    // std::string** tTable = new std::string*[4];
+    //
+    // for(int rows = 0; rows < lines; rows++) {
+    //   tTable[rows] = new std::string[lines];
+    //
+    // }
 
     //Initilize tTable First State
     //tTable[0][3] = "i";
   }
 };
+
+//Adds Data if timestamp < currentTime
+void startImport(Proccessor pArr[4]) {
+  //Declare Variables
+  std::string word = "";
+  std::string line = "";
+  std::ifstream fileArr[3];
+  int rows = 0;
+
+  for(int lcv = 0; lcv < 4; lcv++) {
+    if(rows < pArr[lcv].countLines(pArr[lcv].filePath)) {
+    fileArr[lcv].open(pArr[lcv].filePath, std::ifstream::in);
+
+    //Open File
+    if (fileArr[lcv].is_open()) {
+
+        //Break File into Lines
+        if(getline(fileArr[lcv], line)) {
+          int cols = 0;
+          //Count Number of Lines
+          pArr[lcv].importTable[rows] = new std::string[pArr[lcv].countLines(pArr[lcv].filePath)];
+
+          //Remove Endline char
+          line[line.size()] = ' ';
+
+          //Make Stream Obj of String
+          std::stringstream ssLine(line);
+
+          //Break Lines into Words
+          while(getline(ssLine, word, ' ')) {
+            std::cout<<word <<" (" <<rows <<',' <<cols <<") | ";
+
+            //Assign Word to importTable
+            pArr[lcv].importTable[rows][cols] = word;
+
+            //Initilize State
+            pArr[lcv].importTable[rows][3] = "i";
+
+            cols++;
+          }
+
+          //Check if Record is ready to add
+          while(getTime() < stof(pArr[lcv].importTable[rows][0])) {
+             std::this_thread::sleep_for (std::chrono::nanoseconds(50));
+          }
+          std::cout<<"Processor: " <<lcv <<" | Time: " <<getTime() <<" | adding tag: " <<pArr[lcv].importTable[rows][2] <<std::endl;
+
+          // std::cout<<"\n------------------------------------------------\n";
+        }
+    }
+    else {
+        std::cout << "Error opening file";
+    }
+  }
+}
+  rows++;
+
+  //   //Open File
+  //   if (fileArr[lcv].is_open()) {
+  //       int rows = 0;
+  //
+  //       //Break File into Lines
+  //       while(getline(fileArr[lcv], line)) {
+  //         int cols = 0;
+  //         //Count Number of Lines
+  //         pArr[lcv].importTable[rows] = new std::string[pArr[lcv].countLines(pArr[lcv].filePath)];
+  //
+  //         //Remove Endline char
+  //         line[line.size()] = ' ';
+  //
+  //         //Make Stream Obj of String
+  //         std::stringstream ssLine(line);
+  //
+  //         //Break Lines into Words
+  //         while(getline(ssLine, word, ' ')) {
+  //           // std::cout<<word <<" (" <<rows <<',' <<cols <<") | ";
+  //
+  //           //Assign Word to importTable
+  //           pArr[lcv].importTable[rows][cols] = word;
+  //
+  //           //Initilize State
+  //           pArr[lcv].importTable[rows][3] = "i";
+  //
+  //           cols++;
+  //         }
+  //
+  //         //Check if Record is ready to add
+  //         while(getTime() < stof(pArr[lcv].importTable[rows][0])) {
+  //            std::this_thread::sleep_for (std::chrono::nanoseconds(50));
+  //         }
+  //         std::cout<<"Processor: " <<lcv <<" | Time: " <<getTime() <<" | adding tag: " <<pArr[lcv].importTable[rows][2] <<std::endl;
+  //
+  //         // std::cout<<"\n------------------------------------------------\n";
+  //         rows++;
+  //       }
+  //   }
+  //   else {
+  //       std::cout << "Error opening file";
+  //   }
+  //     fileArr[lcv].close();
+  // }
+  for(int lcv = 0; lcv < 4; lcv++) {
+    fileArr[lcv].close();
+  }
+}
 
 //busRead <physical memory address>
 bool busRd(Proccessor* processors, int activeProcessor, std::string value) {
@@ -129,7 +193,7 @@ bool busRd(Proccessor* processors, int activeProcessor, std::string value) {
 
   // for(int lcv = 0; lcv <= 3; lcv++) {
   //   //Other Cache has Data
-  //   if(processors[activeProcessor].tagTable == processors[lcv] && activeProcessor != lcv) {
+  //   if(processors[activeProcessor].startImport == processors[lcv] && activeProcessor != lcv) {
   //     exclusive = false;
   //   }
   // }
@@ -170,26 +234,28 @@ bool flush(Proccessor* processors, int activeProcessor, std::string value) {
 //Start the program
 int main(int argc, char* argv[])
 {
-  //Global Timer
-  clock_t t;
+  //Create Proccessor Objs
+  //Initilizes number of Lines, startImport, and the filePath
+  Proccessor pArr[4] = {Proccessor("p0.tr"), Proccessor("p1.tr"), Proccessor("p2.tr"), Proccessor("p3.tr")};
 
   //Start Clock
   t = clock();
 
-  //Create Proccessor Objs
-  //Initilizes number of Lines, tagTable, and the filePath
-  Proccessor pArr[4] = {Proccessor("p0.tr"), Proccessor("p1.tr"), Proccessor("p2.tr"), Proccessor("p3.tr")};
+  //Start Processing Transactions
+  startImport(pArr);
+
+
 
   //Move Cache Transaction from importTable to tTable
-  for(int lcv = 0; lcv < 4; lcv++) {
-    std::cout <<"Times: " <<pArr[lcv].importTable[pArr[lcv].valueIndex][0] <<std::endl;
-    //Proccesor Job Triggered
-    if(std::stof(pArr[lcv].importTable[pArr[lcv].valueIndex][0]) > t) {
-      std::cout<<"adding job, current index: " <<pArr[lcv].valueIndex <<std::endl;
-      pArr[lcv].tTable[pArr[lcv].valueIndex] = pArr[lcv].importTable[pArr[lcv].valueIndex];
-      pArr[lcv].valueIndex++;
-    }
-  }
+  // for(int lcv = 0; lcv < 4; lcv++) {
+  //   std::cout <<"Times: " <<pArr[lcv].importTable[pArr[lcv].valueIndex][0] <<std::endl;
+  //   //Proccesor Job Triggered
+  //   if(std::stof(pArr[lcv].importTable[pArr[lcv].valueIndex][0]) > t) {
+  //     std::cout<<"adding job, current index: " <<pArr[lcv].valueIndex <<std::endl;
+  //     pArr[lcv].tTable[pArr[lcv].valueIndex] = pArr[lcv].importTable[pArr[lcv].valueIndex];
+  //     pArr[lcv].valueIndex++;
+  //   }
+  // }
 
   return 0;
 }
